0926533bab3d79632151763003fddef21e388bb0
thobho <tomasz.bar3@gmail.com>
2018-08-01 21:32:54 +0200

    Update Exercise2.java

--- src/Exercises/Exercise2.java
+++ src/Exercises/Exercise2.java
@@ -4,11 +4,11 @@ package Exercises;
 //static void double average(double[] inputArray)
 
 
-
+//Bardzo dobrze rozwiązanie zadanie.
 public class Exercise2 {
 
     public static void main(String[] args) {
-
+        //Jeżeli potrzebujemy losowe dane, to generujmy je za pomocą klasy Randomo
         double[] arr = new double[]{2.5,3.75,6.565,2.85,4.4,3.2,5.9,1.25,6.142,3.14};
 
         average(arr);
@@ -22,7 +22,9 @@ public class Exercise2 {
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
-
+        //Jeżli raz wykorzystujemy jakąś wartość to niekoniecznie musimy ją przypisywać do zmiennej.
+        // Może być tak:
+        // return sum / arr.length;
         double av = sum / array.length;
         return av;
     }
